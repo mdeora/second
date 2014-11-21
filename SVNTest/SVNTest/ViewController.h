e20c1033577c40f82fc38b724a0b245791b8232e
xubojoy <xubojoy1112@163.com>
2014-11-21 09:45:40 +0800

    图片旋转

---
+++ SVNTest/SVNTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SVNTest
+//
+//  Created by myStyle on 14-11-21.
+//  Copyright (c) 2014年 XB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SVNTest/SVNTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SVNTest
+//
+//  Created by myStyle on 14-11-21.
+//  Copyright (c) 2014年 XB. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor purpleColor];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
